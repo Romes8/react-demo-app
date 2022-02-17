8123be713a28862499dac74ec5212c8ed35a94d9
Roman <58208649+Romes8@users.noreply.github.com>
2022-02-17 15:11:41 +0100

    added comments

--- src/components/admin/adminusers.jsx
+++ src/components/admin/adminusers.jsx
@@ -5,12 +5,14 @@ const users = ["John", "Kane", "Tom", "Jerry", "Roman", "Catalina", "Julia"];
 
 
 function AdminUsers() {
-  const [searchParams, setSearchParams] = useSearchParams({});
+  const [searchParams, setSearchParams] = useSearchParams();    //attention to the brackets
 
-  const searchTerm = searchParams.get('name') || '';
+  
+  const searchTerm = searchParams.get('name') || '';  //set the varible name which we are looking for
 
+  //handles the search input change
   const handleSearch = Event => {
-    const name = Event.target.value;
+    const name = Event.target.value;    //const needs to carry the same name as in .get()   /// otherwise it wont work
 
     if (name) {
       setSearchParams({name});
@@ -23,7 +25,7 @@ function AdminUsers() {
   return(
     <div>
       <h1>Admin Users</h1>
-      <input type="text" value={searchTerm} onChange={handleSearch} />
+      <input type="text" value={searchTerm} onChange={handleSearch} />  
       <ul>
         {users.filter(user => user.toLowerCase().includes(searchTerm.toLowerCase()))
         .map((user, i) => (
