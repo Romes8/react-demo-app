35d6dc0dc32d199f42f379b0a700a76c9cd2614a
Roman <58208649+Romes8@users.noreply.github.com>
2022-02-17 15:04:53 +0100

    added searchparams for admin users

--- src/components/admin/adminusers.jsx
+++ src/components/admin/adminusers.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import {useSearchParams} from 'react-router-dom'
+
+const users = ["John", "Kane", "Tom", "Jerry", "Roman", "Catalina", "Julia"];
+
 
 function AdminUsers() {
+  const [searchParams, setSearchParams] = useSearchParams({});
+
+  const searchTerm = searchParams.get('name') || '';
+
+  const handleSearch = Event => {
+    const name = Event.target.value;
+
+    if (name) {
+      setSearchParams({name});
+    }
+    else {
+      setSearchParams({});
+    }
+  };
+
   return(
-    <h1>Admin Users</h1>
+    <div>
+      <h1>Admin Users</h1>
+      <input type="text" value={searchTerm} onChange={handleSearch} />
+      <ul>
+        {users.filter(user => user.toLowerCase().includes(searchTerm.toLowerCase()))
+        .map((user, i) => (
+          <li key={i}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  
   );
 }
 
