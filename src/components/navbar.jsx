5b291d18636e20518f1a12484683aec12a4c5ee6
Roman <58208649+Romes8@users.noreply.github.com>
2022-02-21 17:58:35 +0100

    Added pagination for products

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,21 +3,29 @@ import {Link} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <Link to="/">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link to="/products">Products</Link>
-        </li>
-        <li class="nav-item dropdown">
-          <Link to="/posts">Posts</Link>
-        </li>
-        <li class="nav-item dropdown">
-          <Link to="/admin">Admin</Link>
-        </li>
-      </ul>
+    <div id="navbarSupportedContent">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li class="nav-item active">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link className="nav-link" to="/products" >Products</Link>
+            </li>
+            <li class="nav-item dropdown">
+              <Link className="nav-link" to="/posts">Posts</Link>
+            </li>
+            <li class="nav-item dropdown">
+              <Link className="nav-link" to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,16 +1,50 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
+import Posts from "./posts";
+import Pagination from "./pagination";
+
+
 
 function Products() {
-  
-  const products = [
-    { id: 1, name: "Product 1" },
-    { id: 2, name: "Product 2" },
-    { id: 3, name: "Product 3" }
-  ];
-  
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProducstPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+      setProducts(res.data);
+      setLoading(false);
+    };
+    fetchProducts();
+  }, []);  //empty set to run only once
+
+  // Get current products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct,indexOfLastProduct);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       <h1>Products</h1>
+      
+      <ul className="list-group mb-4">
+        {currentProducts.map(product => (
+          <li key={product.id} className="list-group-item">
+            {product.id} - {product.title}
+          </li>
+        ))}
+      </ul>
+      <Pagination productsPerPage={productsPerPage} totalProducts={products.length} paginate={paginate} />
     </div>
   );
 }
